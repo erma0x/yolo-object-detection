e7468f75bdc111304b21f2c4cf549c7fbc760e3b
erma0x <ermano.buikis@gmail.com>
2022-07-04 11:54:19 +0200

    upload experiments with yolo models

--- yolo_fast_run.py
+++ yolo_fast_run.py
@@ -1,10 +1,10 @@
 import cv2
 import sys
 import torch
-model = torch.hub.load('ultralytics/yolov5', 'yolov5x6')
+model = torch.hub.load('ultralytics/yolov5', 'yolov5l')
 
 PATH_TO_IMAGE = sys.path[0]+'/img/people.jpeg'
 img = cv2.imread(PATH_TO_IMAGE)
-results = model(img, size=1200)
+results = model(img, size=640)
 results.print() 
 results.save()
